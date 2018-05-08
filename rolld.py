a1deceebaf6e6a1f3f611852b9a8ad299ea31876
Jared Halpern <jared@jaredhalpern.com>
2018-05-08 19:32:37 -0400

    test

--- rolld.py
+++ rolld.py
@@ -2,12 +2,10 @@ import os
 import re #regex
 from slackclient import SlackClient
 
-# slack_token = os.environ["SLACK_API_TOKEN"]
 slack_client = SlackClient(os.environ.get('SLACK_ROLLD_TOKEN'))
 
 slack_client.api_call(
-  "chat.postEphemeral",
+  "chat.postMessage",
   channel="programming",
   text="Greetings! :tada:",
-  user="jared"
 )
